57fa24e7bfb342a90a35349d5a162c50423a4c87
hutch2323 <marcus.hutchings30@gmail.com>
2022-02-11 11:23:55 -0330

    Added backend API calls to dummy data file. Updated App.js on front end to display dummy data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import Container from 'react-bootstrap/Container';
+import { BsClock } from "react-icons/bs"; 
 
 function App() {
+
+  const [twitchDrops, setTwitchDrops] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/twitchDrops')
+      .then((response) => response.json())
+      .then(setTwitchDrops)
+  }, []);
+
+  if( twitchDrops == null) return null;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container className="bg-light">
+        <h1>Twitch Drops</h1>
+        { twitchDrops.map((twitchDrop, i) => { return <TwitchDrops key={i} info={twitchDrop}></TwitchDrops>}) }
+      </Container>
     </div>
   );
+}
+
+function TwitchDrops(props) {
+  console.log(props);
+  return (
+    <>
+      <div style={{display: "inline-block", margin: "2em"}}>
+        <h2 className="mt-5">{props.info.streamer_name}</h2>
+        <img rounded="true" src={props.info.item_icon}></img>
+        <h3 className="fs-4 text-start">{props.info.item_name}</h3>
+        <p><BsClock style={{paddingRight: "5px"}}/>{props.info.unlock_condition}</p>
+      </div>
+    </>
+  )
 }
 
 export default App;
