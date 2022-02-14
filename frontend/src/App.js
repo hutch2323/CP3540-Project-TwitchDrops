5f3e4477c282d1fb6c4bad95416f69418b243b69
Denis Murphy <djmurphy2001@gmail.com>
2022-02-14 15:29:14 -0330

    re-added drops panel generation code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Container, Image } from 'react-bootstrap';
-import { BsClock } from "react-icons/bs"; 
+import { Container, Image } from 'react-bootstrap'; 
 import { Current, Past, FAQ } from "./pages";
 
 function App() {
@@ -36,8 +35,19 @@ function App() {
         </Routes>
       </Container>
     </div>
+      );
+    }
     
-  );
-}
+    export function TwitchDrops(props) {
+      console.log(props);
+      return (
+        <Container className="text-light border-primary rounded-3 p-3" style={{backgroundColor: "blue"}}>
+          <h2 className="mt-5">{props.info.streamer_name}</h2>
+          <Image src={props.info.item_icon} thumbnail="true" />
+          <h3 className="fs-4 text-start">{props.info.item_name}</h3>
+          <p><BsClock style={{paddingRight: "5px"}}/>{props.info.unlock_condition}</p>
+        </Container>
+      )
+    }
 
 export default App;
